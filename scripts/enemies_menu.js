e7804b6e47eda6b4373d77d60e3247a7e34d9c6f
hot-potato2010 <149298484+hot-potato2010@users.noreply.github.com>
2024-05-27 21:24:57 +0400

    first commit

---
+++ scripts/enemies_menu.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded",function(){
+    document.querySelector("#enemies_menu").addEventListener("click",function(){
+        document.querySelector(".enemies_menu").classList.toggle("is-active")
+    })
+})
